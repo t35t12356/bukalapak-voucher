7b42e3bf58f3b35efe3b7fe60efda43d454d6530
t35t12356 <43946872+t35t12356@users.noreply.github.com>
2018-10-10 15:19:04 +0700

    Update cdy.js

--- cdy.js
+++ cdy.js
@@ -7,8 +7,8 @@ const randstr = require('randomstring');
 const inquirer = require('inquirer');
 const fs = require('fs');
 // 
-const base = "BLMOIV";
-let length = 4;
+const base = "BLMOIV4CMS";
+let length = 0;
 //
 async function check(code) {
 	const option = {
