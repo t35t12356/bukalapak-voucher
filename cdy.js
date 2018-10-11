87bb8446def9a70edac26b46c7988503db6d83f5
t35t12356 <43946872+t35t12356@users.noreply.github.com>
2018-10-11 14:33:20 +0700

    Update cdy.js

--- cdy.js
+++ cdy.js
@@ -7,7 +7,7 @@ const randstr = require('randomstring');
 const inquirer = require('inquirer');
 const fs = require('fs');
 // 
-const base = "BLMOIN";
+const base = "BLMOIC";
 let length = 4;
 //
 async function check(code) {
